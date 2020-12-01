7ac31cc8ebd6153a1332930048602ddc67eda278
friendship41 <stage41@nate.com>
2020-12-01 22:40:46 +0900

    점심식사 추가 기능 구현

--- app/src/main/java/com/friendship41/life_util/AddLunchActivity.kt
+++ app/src/main/java/com/friendship41/life_util/AddLunchActivity.kt
@@ -1,11 +1,17 @@
 package com.friendship41.life_util
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.RadioButton
 import android.widget.Toast
+import com.friendship41.life_util.common.getRoomFileMap
 import com.friendship41.life_util.common.log
+import com.friendship41.life_util.common.saveFile
+import com.jayway.jsonpath.JsonPath
 import kotlinx.android.synthetic.main.activity_add_lunch.*
+import java.io.File
+import java.lang.Exception
 
 class AddLunchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,16 +22,21 @@ class AddLunchActivity : AppCompatActivity() {
             val restaurantName: String = edt_restaurant_name.text.toString()
             if (restaurantName.isEmpty()) {
                 log().info("식당명이 없음...")
-                Toast.makeText(this.applicationContext, "식당명을 입력해야 합니다.", Toast.LENGTH_SHORT)
+                Toast.makeText(this.applicationContext, "식당명을 입력해야 합니다.", Toast.LENGTH_SHORT).show()
                 edt_restaurant_name.requestFocus()
                 return@setOnClickListener
             }
             val checkedPreference = findViewById<RadioButton>(rdg_preference.checkedRadioButtonId)
             log().info("add lunch restaurant name=$restaurantName, checkedPreference=${checkedPreference.text}")
 
-            this.intent.putExtra("restaurantName", restaurantName)
-            this.intent.putExtra("checkedPreference", checkedPreference.text.toString())
-            this.setResult(RESULT_CODE_ADD_LUNCH_SUCCESS, this.intent)
+            try {
+                saveLunchFile(this.applicationContext, "temp", restaurantName, checkedPreference.text.toString())
+            } catch (e: DuplicateRestaurantNameException) {
+                log().info("식당명이 중복...")
+                Toast.makeText(this.applicationContext, e.message, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             finish()
         }
 
@@ -33,4 +44,24 @@ class AddLunchActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    private fun saveLunchFile(context: Context, roomName: String, restaurantName: String, checkedPreference: String) {
+        var roomFile = getRoomFileMap(context)["room_$roomName"]
+        if (roomFile == null) {
+            saveFile(context, "room_$roomName", "{}")
+            roomFile = File(context.filesDir, "room_$roomName")
+        }
+        val roomMap = JsonPath.parse(roomFile).json<HashMap<String, Any>>()
+        if (roomMap.containsKey(restaurantName)) {
+            throw DuplicateRestaurantNameException("[$restaurantName] 식당명이 중복됩니다.")
+        }
+        roomMap[restaurantName] = mapOf(
+            Pair("checkedPreference", checkedPreference),
+            Pair("count", 0)
+        )
+        saveFile(context, "room_$roomName", JsonPath.parse(roomMap).jsonString())
+        log().info(JsonPath.parse(roomMap).jsonString())
+    }
 }
+
+class DuplicateRestaurantNameException(message: String): Exception(message)
--- app/src/main/java/com/friendship41/life_util/common/FileUtil.kt
+++ app/src/main/java/com/friendship41/life_util/common/FileUtil.kt
@@ -3,9 +3,15 @@ package com.friendship41.life_util.common
 import android.content.Context
 import java.io.File
 
-fun saveFile(context: Context, filename: String, jsonContent: String) {
-    context.openFileOutput(filename, Context.MODE_PRIVATE)
-        .use { it.write(jsonContent.toByteArray()) }
-}
+fun saveFile(context: Context, filename: String, jsonContent: String) = context
+    .openFileOutput(filename, Context.MODE_PRIVATE)
+    .use { it.write(jsonContent.toByteArray()) }
 
 fun getFile(context: Context, filename: String): File = File(context.filesDir, filename)
+
+fun getFileNameList(context: Context): Array<String> = context.fileList()
+
+fun getRoomFileMap(context: Context): Map<String, File> = getFileNameList(context)
+    .filter { it.contains("room") }
+    .map { it to File(context.filesDir, it) }
+    .toMap()
